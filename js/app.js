3f9d35350f5cf574ecccf6b37626b3b6f8c6f080
ptarnaud <patrick.tarnaud@systeme-u.fr>
2018-08-29 13:59:28 +0200

    composant message et counter

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
+let message = {
+    props: {
+        type: {
+            type: String,
+            default: 'success'
+        },
+        message: String
+    },
+    template: `<div :class='type'>{{message}}</div>`
+}
+
+let counter = {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        incrementer: function () {
+            this.count++
+        }
+    },
+    template: `<div>
+        <span>{{ count }}</span>
+        <button @click='incrementer'>Incrémenter</button>
+        </div>`
+}
+
 let vm = new Vue({
-  el: '#app',
-  data: {
-    message: ''
-  }
+    el: '#app',
+    components: {
+        message,
+        counter
+    },
+    data: {
+        msgSuccess: '',
+        msgError: ''
+    }
 })
