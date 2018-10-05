c098d60b69684e74a264dfc381b441b840e9e073
ptarnaud <patrick.tarnaud@systeme-u.fr>
2018-10-05 16:12:02 +0200

    svg

--- js/app.js
+++ js/app.js
@@ -1,39 +1,51 @@
 let message = {
-    props: {
-        type: {
-            type: String,
-            default: 'success'
-        },
-        message: String
+  props: {
+    type: {
+      type: String,
+      default: 'success'
     },
-    template: `<div :class='type'>{{message}}</div>`
+    message: String,
+    header: String
+  },
+  template: `<div :class='type'>
+                <i class="close icon" style="width: 10px; height: 10px">X</i>
+                <div class="header">{{header}}</div>
+                <span>{{message}}</span>
+              </div>`
 }
 
 let counter = {
-    data: function () {
-        return {
-            count: 0
-        }
-    },
-    methods: {
-        incrementer: function () {
-            this.count++
-        }
-    },
-    template: `<div>
-        <span>{{ count }}</span>
-        <button @click='incrementer'>Incrémenter</button>
-        </div>`
+  props: {
+    start: {
+      type: Number,
+      default: 0
+    }
+  },
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  mounted() {
+    this.count = this.start
+
+  },
+  methods: {
+    incrementer() {
+      this.count++
+    }
+  },
+  template: `<button @click='incrementer'>{{ count }}</button>`
 }
 
 let vm = new Vue({
-    el: '#app',
-    components: {
-        message,
-        counter
-    },
-    data: {
-        msgSuccess: '',
-        msgError: ''
-    }
+  el: '#app',
+  components: {
+    message,
+    counter
+  },
+  data: {
+    msgSuccess: 'Mon message de succès',
+    msgError: 'Mon message d\'erreur'
+  }
 })
